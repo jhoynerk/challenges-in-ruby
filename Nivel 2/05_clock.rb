9b069e6e1e305b5f2b2fe92d978d5a09bcf13d45
Jhoynerk Caraballo <jhoynerk@gmail.com>
2017-06-24 20:28:51 -0300

    solution challenge 5 - nivel 2

--- Nivel 2/05_clock.rb
+++ Nivel 2/05_clock.rb
@@ -1,24 +1,27 @@
 require "test/unit"
 
 class Clock
-  attr_accessor :seconds, :clock, :total_sum_leds
-
+  attr_accessor :seconds, :time, :total_sum_leds, :clock, :total
 
   def initialize(seconds)
     @seconds = seconds
-    @clock = { hours: 0, minutes: 0, seconds: 0}
+    @time = { hours: 0, minutes: 0, seconds: 0}
+    @clock = generate_clock('000000')
+    @total = total_clock_points
   end
 
   def sum_leds_on
-    hours_minutes_seconds.each_with_index do |time, key|
+    hours_minutes_seconds.reverse.each_with_index do |time, key|
       sum_led_on(time, key) if time > 0
     end
+    total
   end
 
   def sum_led_on(time, key)
     seconds_to_time = all_seconds(time, key)
-    (0..seconds_to_time).each do |second|
-      p second
+    (1..seconds_to_time).each do |second|
+      add_second_to_clock
+      @total += total_clock_points
     end
   end
 
@@ -26,29 +29,41 @@ class Clock
     time * ((key != 0) ? 60 : 1)
   end
 
-  def generate_clock(time)
-    time.map { SevenSegment.new(0).total_leds }
+  def generate_clock(number)
+    number.chars.map { |n| SevenSegment.new(n) }
+  end
+
+  def total_clock_points
+    @clock.map { |c| c.total_leds }.reduce(:+)
   end
 
   def hours_minutes_seconds
-    formatted_duration.each_with_index do |time, index|
-      clock[clock.key(index)] = time
+    formatted_duration.each_with_index do |t, index|
+      time[time.key(index)] = t
     end
+  end
+
+  def add_second_to_clock
+    number = clock_to_i + 1
+    number = number.to_s.rjust(6, '0')
+    @clock = generate_clock(number)
+  end
+
+  def clock_to_i
+    @clock.map { |c| c.number }.join('').to_i
   end
 
   def formatted_duration
     [60,60].reduce([seconds]) { |m,o| m.unshift(m.shift.divmod(o)).flatten }
   end
-
-  def sum_clocks_in_secod
-    @clock.reduce(:+)
-  end
 end
 
 class SevenSegment
+  attr_accessor :number
+
   LED_LIGHTS = [ 6, 2, 5, 5, 4, 5, 6, 3, 7, 5 ]
   def initialize(number)
-    @number = number
+    @number = number.to_i
   end
 
   def total_leds
@@ -60,10 +75,10 @@ end
 class ModulinoTest < Test::Unit::TestCase
   def test_clock
     seconds = 0
-    #assert_equal( 36 , Clock.new(seconds).sum_leds_on)
-    #assert_equal( 36 , Clock.new(seconds).sum_leds_on)
+    assert_equal( 36 , Clock.new(seconds).sum_leds_on)
+
+    seconds = 4
+    assert_equal( 172 , Clock.new(seconds).sum_leds_on)
   end
 end
 
-# Manual Testing
-p Clock.new(119).sum_leds_on
